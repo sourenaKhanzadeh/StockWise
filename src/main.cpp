e347da17af2819828f9d7f485431f4e4c8b239ce
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-02-09 19:54:31 -0500

    Api call made successfully

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/users/sourenaKhanzadeh"});
+
+    std::cout << r.status_code << std::endl;
+    std::cout << r.header["content-type"] << std::endl;
+    std::cout << r.text << std::endl;
     return 0;
 }
